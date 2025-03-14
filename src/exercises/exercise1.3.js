16adf43e43c6d2f5de1160d588ce7e3489e94489
Alejandro García Serrano <agscm2@gmail.com>
2025-03-14 12:34:00 +0100

    Upload Exercises 1 - 2

---
+++ src/exercises/exercise1.3.js
@@ -0,0 +1,10 @@
+/**
+ * Ejercicio 3. Crea una web en HTML que muestre 4 cuadrados rellenos de color hex. #3b78d7. Usando
+    CSS centrar los cuadrados horizontal y verticalmente con una separación de 10px.
+    Una vez esté listo el código CSS elimina los elementos HTML y crea una función que le pasemos el
+    número de cuadrados y añada todos los elementos HTML necesarios para mostrar el mismo resultado
+    que antes. Ahora crea las clases necesarias en CSS para que los cuadrados alternen 3 colores
+    sucesivamente y actualiza tu función para que aplique las clases.
+    Nota: para definir las clases CSS que va a tener un elemento en Javascript usa la propiedad “className”
+    de dicho elemento asignándole una cadena de texto con el nombre de las clases.
+ */
--- src/main.js
+++ src/main.js
@@ -1,23 +1,9 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from './vite.svg'
+import { main1_1 } from './exercises/exercise1.1'
+import { main1_2 } from './exercises/exercise1.2'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://github.com/Alegarse" target="_blank">
-      <img src="https://raw.githubusercontent.com/Alegarse/Dev_Utils/9574e643ce173b9a5c06c714c4c5ef7e532bcf92/logo_ags.svg" class="logo" alt="AGS logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>JavaScript Advance Exercises</h1>
-    <div class="exercises-grid">
-    </div>
-    <div class="card">
-      <button id="repo-button" type="button"><a href="https://github.com/Alegarse/B17-mod3-js-advance-exercises" target="_blank" title="Open link to repository">Repositorio</a></button>
-    </div>
-  </div>
-`
+// Exercise 1.1
+//main1_1()
+
+// Exercise 1.2
+main1_2()
