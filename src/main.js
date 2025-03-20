db795cb3286a163ae1737dde503a333009f7d7e2
Alejandro García Serrano <agscm2@gmail.com>
2025-03-21 00:24:33 +0100

    fix

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ let indexBase = `
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
     <h1>JavaScript Advanced Exercises</h1>
-    <p>Debes habilitar la consola de insepcción para visualizar los resultados de algunos ejercicios cuando hagas click en cada botón correspondiente</p>
+    <p>Debes habilitar la consola de inspección para visualizar los resultados de algunos ejercicios cuando hagas click en cada botón correspondiente</p>
     <div class="exercises-grid">
       <div class="grid-tema">
         <h3>Tema 1</h3>
